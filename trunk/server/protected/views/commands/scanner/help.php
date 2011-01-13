ec7da23839db46ff076e8d2c89e63f90b87119f0
mail@icesheep.de <mail@icesheep.de@143e3bdb-efc2-74f8-7432-d731a236869c>
2011-01-13 21:50:39 +0000

    Collection scanner is now a real command line application - Improved progress indicator - Checks if exiftool is available before starting to scan Can be invoked by issuing ./musikdbadm scanner [full|update] in the protected/ directory

---
+++ trunk/server/protected/views/commands/scanner/help.php
@@ -0,0 +1,7 @@
+
+Please specify an action.
+
+  full    do a full scan (current collection will be discarded and
+          rebuilt from scratch)
+  update  do an update scan (current collection will be updated)
+
